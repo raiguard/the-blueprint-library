34a2e080c8e23b94a6a6d83d11f7abd4f7f81986
raiguard <caleb.heuer@gmail.com>
2020-07-23 16:38:27 -0600

    Add database seed

---
+++ db/seed.sql
@@ -0,0 +1,33 @@
+create table users (
+  id serial primary key,
+  username varchar(30),
+  password text,
+  avatar text,
+  is_admin boolean
+);
+
+create table posts (
+  id serial primary key,
+  author_id int references users(id),
+  title varchar(80),
+  img text,
+  description varchar(3000),
+  timestamp int
+);
+
+create table comments (
+  id serial primary key,
+  post_id int references posts(id),
+  author_id int references users(id),
+  content varchar(500),
+  timestamp int
+);
+
+create table records (
+  id serial primary key,
+  name text,
+  description text,
+  post_id int references posts(id),
+  book_id int references records(id),
+  string text
+);
