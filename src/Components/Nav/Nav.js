77f9bcb7ff264ffa180fb32c237b42c1acc4c543
raiguard <caleb.heuer@gmail.com>
2020-07-24 12:52:58 -0600

    Nav/auth basic layout and visibility

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,79 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setUser } from "../../redux/reducer";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default () => {
-  const dispatch = useDispatch();
   const userData = useSelector((state) => state.user);
+  const [authType, setAuthType] = useState(null);
+
+  const toggleAuthType = (type) => {
+    if (authType !== type) {
+      setAuthType(type);
+    } else {
+      setAuthType(null);
+    }
+  };
 
   return (
     <nav>
-      <p>{userData.username}</p>
+      <div className="nav-left-part">
+        <Link to="/">
+          <h1>The Blueprint Library</h1>
+        </Link>
+        {userData.username && (
+          <Link to="/create">
+            <label>Create</label>
+          </Link>
+        )}
+        <Link to="/about">
+          <label>About</label>
+        </Link>
+      </div>
+      <div>
+        {userData.username ? (
+          <>
+            <Link to="/profile">
+              <label>Profile</label>
+            </Link>
+            <label>Sign out</label>
+          </>
+        ) : (
+          <>
+            <label onClick={() => toggleAuthType("Sign in")}>Sign in</label>
+            <label onClick={() => toggleAuthType("Register")}>Register</label>
+          </>
+        )}
+      </div>
+      {authType && <Auth type={authType} />}
     </nav>
+  );
+};
+
+export const Auth = (props) => {
+  const checkCredentials = () => {
+    alert(props.type);
+  };
+
+  return (
+    <section className="auth">
+      <AuthInput label="Username:" />
+      <AuthInput label="Password:" onConfirm={checkCredentials} />
+      <button onClick={checkCredentials}>{props.type}</button>
+    </section>
+  );
+};
+
+const AuthInput = ({ label, onConfirm }) => {
+  const [value, setValue] = useState("");
+
+  const checkForEnter = (e) => {
+    if (onConfirm && e.which === 13) onConfirm();
+  };
+
+  return (
+    <div className="auth-input">
+      <h2>{label}</h2>
+      <input onChange={(e) => setValue(e.target.value)} value={value} onKeyPress={checkForEnter} />
+    </div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 export default (
   <Switch>
-    <Route exact path="/" render={() => <div>Home</div>} />
-    <Route path="/new" render={() => <div>New</div>} />
-    <Route path="/edit" render={() => <div>Edit</div>} />
-    <Route path="/post" render={() => <div>Post</div>} />
-    <Route path="/profile" render={() => <div>Profile</div>} />
+    <Route exact path="/" component={Home} />
     <Route render={() => <div>404 not found</div>} />
   </Switch>
 );
