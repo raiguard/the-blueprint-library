489e2e35b61a948e76c2e26d4d58ae088cf9ff9f
raiguard <caleb.heuer@gmail.com>
2020-07-27 13:48:38 -0600

    Style experiments

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,61 @@
 import React from "react";
 
+const debugPosts = [
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  },
+  {
+    title: "Space Science",
+    description: "My awesome space science setup"
+  }
+];
+
 export default () => {
   return (
     <main className="home">
       <section className="search">
         <input placeholder="Search..." />
+      </section>
+      <section className="grid-view">
+        {debugPosts.map((post, i) => (
+          <div className="post-card" key={i}>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+          </div>
+        ))}
       </section>
     </main>
   );
