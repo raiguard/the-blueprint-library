aefe1b4ab8a47081cf7dd5eb50a6b2e13e3b2301
raiguard <caleb.heuer@gmail.com>
2020-07-29 16:32:06 -0600

    Posts are clickable

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import { Link } from "react-router-dom";
 
 export default () => {
   const [posts, setPosts] = useState([]);
@@ -21,10 +22,10 @@ export default () => {
       </section>
       <section className="grid-view">
         {posts.map((post, i) => (
-          <div className="post-card" key={i}>
+          <Link key={i} className="post-card" to={`/posts/${post.id}`}>
             <h1>{post.title}</h1>
             <p>{post.description}</p>
-          </div>
+          </Link>
         ))}
       </section>
     </main>
