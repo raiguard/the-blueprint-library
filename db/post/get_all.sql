a7c209ef12ba3d995167d6b5fd123c175495940b
raiguard <caleb.heuer@gmail.com>
2020-07-30 15:54:23 -0600

    Include author name and avatar in post queries

--- db/post/get_all.sql
+++ db/post/get_all.sql
@@ -1,3 +1,5 @@
-select *
-from posts
-where lower(title) like '%' || lower($1) || '%';
+select p.*, u.username as author_username, u.avatar as author_avatar
+from posts p
+inner join users u
+on p.author_id = u.id
+where lower(p.title) like '%' || lower($1) || '%';
--- db/post/get_one.sql
+++ db/post/get_one.sql
@@ -1,3 +1,5 @@
-select *
-from posts
-where id = $1;
+select p.*, u.username as author_name, u.avatar as author_avatar
+from posts p
+inner join users u
+on p.author_id = u.id
+where p.id = $1;
