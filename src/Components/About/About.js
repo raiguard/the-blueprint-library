ea42dd689268f393675ff43e2f4cbded70695e7e
raiguard <caleb.heuer@gmail.com>
2020-07-31 12:05:09 -0600

    Basic about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default () => {
-  return <main className="about">Hullo! This is Scott Manley here</main>;
+  return (
+    <main className="about">
+      <p>
+        The Blueprint Library is a place to share your Factorio blueprints. It is designed with speed and ease-of-use in
+        mind, built on React and Node. While this site started as a school project, I intend to make it the central
+        place to share and download Factorio blueprints. As I learn more about web development, this site will gain more
+        features, more stability, and most importantly, mod support!
+      </p>
+      <p>
+        <a href="https://github.com/raiguard/the-blueprint-library">Visit us on GitHub</a>
+      </p>
+    </main>
+  );
 };
