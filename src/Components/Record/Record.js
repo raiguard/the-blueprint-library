c81d8475c38f7c4bafe6beb411254bb1a7caf37e
raiguard <caleb.heuer@gmail.com>
2020-08-04 16:38:34 -0600

    Slightly better children layout for records

--- src/Components/Record/Record.js
+++ src/Components/Record/Record.js
@@ -6,16 +6,18 @@ const Record = ({ data, index, remove }) => {
   const { type, name, description, string, children } = data;
   return (
     <div className="record">
+      <div>
+        <h1>{name}</h1>
+        <h2>{type}</h2>
+        <p>{description}</p>
+        <input value={string} readOnly />
+      </div>
       {remove && <button onClick={() => remove(index)}>Remove</button>}
-      <h1>{name}</h1>
-      <h2>{type}</h2>
-      <p>{description}</p>
-      <input value={string} readOnly />
       {children && children.length > 0 && (
-        <>
+        <div className="children-container">
           <button onClick={() => setShowChildren(!showChildren)}>Toggle children</button>
           {showChildren && children.map((child, i) => <Record key={i} data={child} />)}
-        </>
+        </div>
       )}
     </div>
   );
