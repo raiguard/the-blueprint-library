7ea87e68591e0ff449367d17be2aa810dc69c5f8
raiguard <caleb.heuer@gmail.com>
2020-08-05 12:42:02 -0600

    Display children as indented instead of inset

--- src/Components/Record/Record.js
+++ src/Components/Record/Record.js
@@ -5,16 +5,18 @@ const Record = ({ data, index, remove }) => {
 
   const { type, name, description, string, children } = data;
   return (
-    <div className="record">
-      <div>
-        <h1>{name}</h1>
-        <p>{description}</p>
-        <input value={string} readOnly />
-        <button onClick={() => setShowChildren(!showChildren)}>Toggle children</button>
+    <div className="record-container">
+      <div className="record">
+        <div>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <input value={string} readOnly />
+          {type === "book" && <button onClick={() => setShowChildren(!showChildren)}>Toggle children</button>}
+        </div>
+        {remove && <button onClick={() => remove(index)}>Remove</button>}
       </div>
-      {remove && <button onClick={() => remove(index)}>Remove</button>}
       {children && children.length > 0 && (
-        <div className="inset-panel children-container">
+        <div className="children-container">
           {showChildren && children.map((child, i) => <Record key={i} data={child} />)}
         </div>
       )}
